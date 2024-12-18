830bc80765ba608633e2fa0786068c6284046454
bourb0n1412 <heikoho1@icloud.com>
2024-12-18 15:12:43 +0100

    Implemented Login Page

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    java
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -18,7 +19,20 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // AES für Verschlüsselung
+    implementation("org.apache.commons:commons-crypto:1.1.0")
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Testen
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
